1e6b8d020c889d1850a196b56d0bb4d194b8325a
sarodhesachin <sachin.s@hashedin.com>
2019-09-06 12:17:50 +0530

    added cars list component
    Signed-off-by: sarodhesachin <sachin.s@hashedin.com>

--- src/Container/Dashboard/CarsList/CarsList.js
+++ src/Container/Dashboard/CarsList/CarsList.js
@@ -1,15 +1,88 @@
 import React from 'react';
 
+import constants from '../../../constants'
 import './CarsList.css'
 
-class CarsList extends React.Component {
+class CarsListContainer extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      carDetails: []
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchCarDetails()
+  }
+
+  fetchCarDetails = () => {
+    fetch(constants.CARS_INVENTRY_URL).then(rawResponse => {
+      return rawResponse.json()
+    }).then(details => {
+      console.log(details)
+      this.setState({
+        carDetails: details.Response.VehicleSearch.Vehicles.Vehicle
+      })
+    })
+  }
+
   render() {
     return (
-      <div className='CarsList'>
-        CarsList
+      <div className='CarsListContainer'>
+        <div>
+          <p>DASHBOARD</p>
+        </div>
+        <Cars carDetails={this.state.carDetails}/>
       </div>
     )
   }
 }
 
-export default CarsList;
+class Cars extends React.Component {
+  render() {
+
+    let cars = <div></div>
+    if (Array.isArray(this.props.carDetails)) {
+      cars = <div className='CarsList'>
+        {
+          this.props.carDetails.map(car => {
+            return (
+              <Car 
+                model={car.Model} 
+                imageToken={car.ImageToken} 
+                vin={car.Vin} 
+                price={car.Pricing.MSRP}
+              />
+            )
+          })
+        }
+      </div>
+
+    }
+
+    return (
+      <div>{cars}</div>
+    )
+  }
+}
+
+class Car extends React.Component {
+  render() {
+    let imgSource = `http://build.ford.com/dig/Ford/${this.props.model.ModelName}/${this.props.model.Year}/HD-FULL/${this.props.imageToken}/EXT/1/vehicle.png?r=NaN`
+    console.log(imgSource)
+
+    return (
+      <div className='Car'>
+        <img src={imgSource}/>
+        <label>{`${this.props.model.Year} ${this.props.model.ModelName} ${this.props.model.Make}`}</label>
+        <hr />
+        <p style={{fontWeight: "bold"}}>Net Price: ${this.props.price}</p>
+        <p>{"VIN " + this.props.vin}</p>
+        <button className='button'>View Details</button>
+      </div>
+    )
+  }
+}
+
+export default CarsListContainer;
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 module.exports = {
-  FILTER_URL: 'https://wwwqa.servicesus.ford.com/inventory/FilterItems.json?make=Ford&model=Mustang&year=2017'
+  FILTER_URL: 'https://wwwqa.servicesus.ford.com/inventory/FilterItems.json?make=Ford&model=Mustang&year=2017',
+  CARS_INVENTRY_URL: 'http://wwwqa.servicesus.ford.com/inventory/Search.json?make=Ford&model=F-150;Fiesta;Mustang;Fusion;Transit&year=2017&dealerPACode=05453;09158&postalCode=90210'
 }
